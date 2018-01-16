556683ee1432189ff42787232fa27cddf8725fd9
Marco Antonio <ma.souza.junior@gmail.com>
2018-01-16 21:13:17 +0100

    ENH : auto clone oh-my-zsh

--- apply.sh
+++ apply.sh
@@ -1,4 +1,7 @@
 # Just copy everything to $HOME
 echo "Setting up zsh"
+# Install configs
 cp -r .aliases .neovimrc .zsh-update .zshrc .oh-my-zsh $HOME
+# Clone oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 echo "Done "
