df84ad70b43da65b95323fbf08ca298bc79dafdd
Marco Antônio <ma.souza.junior@gmail.com>
2018-07-27 20:35:33 -0300

    ENH : Improve apply script

--- apply.sh
+++ apply.sh
@@ -1,8 +1,10 @@
 # Just copy everything to $HOME
 echo "Setting up zsh"
+
 # Install configs
 cp -r .aliases .neovimrc .zsh-update .zshrc $HOME
 echo " • Installing configurations..."
+
 # Clone oh-my-zsh
 dest=$HOME/.oh-my-zsh
 if [ ! -e $dest ];
@@ -10,7 +12,17 @@ then
   echo " • Cloning Oh-My-Zsh in $dest..."
   git clone https://github.com/robbyrussell/oh-my-zsh.git $dest
 fi
+
 echo " • Applying configs to current section..."
 source ~/.{aliases,neovimrc,zsh-update,zshrc}
+
+echo " • Applying zsh to open instead of bash..."
+echo -e "\nzsh; exit" >> ~/.bashrc
+
+echo " • Creating local npm folder..."
+make ~/.npm-global
+
+echo " • Install virtualenv as dependencies..."
+pip install virtualenv{,wrapper}
+
 echo " • Done!"
-echo "Done "
